c9db2519dd3f87df05c46fa7d520e0407a953946
JudLup Luna <judlup@gmail.com>
2023-03-14 23:55:48 -0500

    .

--- enlaces/crear/crear.js
+++ enlaces/crear/crear.js
@@ -1,3 +1,8 @@
+const AWS = require("aws-sdk")
+AWS.config.update({ region: "us-east-1" })
+
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" })
+
 module.exports.handler = async (event) => {
   // Aqui va el proceso
   /*
@@ -13,12 +18,23 @@ module.exports.handler = async (event) => {
   const enlace_corto = event.queryStringParameters.enlace_corto
   const fecha_expiracion = event.queryStringParameters.fecha_expiracion
   const usuario = "12345"
+  let tableNames
+  // Obtener tablas creadas en DynamoDB
+  ddb.listTables({ Limit: 10 }, function (err, data) {
+    if (err) {
+      console.log("Error", err.code)
+    } else {
+      tableNames = data.TableNames
+      console.log("Table names are ", data.TableNames)
+    }
+  })
 
   const obj = {
-    enlace_original,
-    enlace_corto,
-    fecha_expiracion,
-    usuario,
+    //enlace_original,
+    //enlace_corto,
+    //fecha_expiracion,
+    //usuario,
+    tableNames,
   }
   //
   return {
