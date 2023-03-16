364f1bf74e6a7c5451a6fdc6455c7cb62ab6827a
JudLup Luna <judlup@gmail.com>
2023-03-16 00:28:48 -0500

    Agregamos el endpoint de actualizar enlace

---
+++ enlaces/actualizar/actualizar.js
@@ -0,0 +1,35 @@
+const AWS = require("aws-sdk")
+
+AWS.config.update({ region: "us-east-1" })
+
+const dynamodb = new AWS.DynamoDB.DocumentClient()
+
+module.exports.handler = async (event) => {
+  const id = event.pathParameters.id
+  const enlace_corto = event.queryStringParameters.enlace_corto
+
+  await dynamodb
+    .update({
+      TableName: "enlacesTable",
+      UpdateExpression: "set enlace_corto = :enlace_corto",
+      ExpressionAttributeValues: {
+        ":enlace_corto": enlace_corto,
+      },
+      Key: {
+        id: id,
+      },
+      ReturnValues: "ALL_NEW",
+    })
+    .promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        data: "Enlace actualizado",
+      },
+      null,
+      2
+    ),
+  }
+}
